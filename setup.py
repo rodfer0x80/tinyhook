5c818fa227df02e0a606e38e5383dabca26ee38e
trevalkov <rodrigolf080@gmail.com>
2022-01-06 19:14:26 +0000

    .

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+
+
+
+if __name__ == '__main__':
+    # get passwd for CH_PASSWD
+    # get IP and PORT to host server
+    # change IP and PORT in all files
+    # alert when done what
+    exit(0)
